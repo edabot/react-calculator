8afb1c8e63bac6af0cab8399512b5fb1f87f6b94
Ed Lewis <ed.lewis@gmail.com>
2017-07-07 09:33:48 -0700

    update

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React Calculator</h2>
-        </div>
         <Calculator />
       </div>
     );
